8fa7648a59042f376ed5f72855cd46024587ff1f
Federico Dutto <E-federico.dutto@msc.com>
2023-07-05 22:49:28 +0200

    SBLOCK falling down

--- src/FrmTetris.java
+++ src/FrmTetris.java
@@ -1,14 +1,97 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FrmTetris extends JFrame implements ActionListener {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private Timer timer = null;
+    public static JCartesian graph = null;
+    public static JLabel lblCounter = null;
+    private JButton btnStartStop = null;
+
+    public FrmTetris() {
+
+        setSize(600, 750);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Tetris");
+
+        initUI();
+
+        this.timer = new Timer(1000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Round.next();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void initUI() {
+
+        pnlNorth();
+        pnlCenter();
+        pnlSouth();
+    }
+
+    private void pnlNorth() {
+
+        JPanel pnlNorth = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JLabel lblTetris = new JLabel("PLAY TETRIS!");
+        pnlNorth.add(lblTetris);
+        this.add(pnlNorth, BorderLayout.NORTH);
+    }
+
+    private void pnlCenter() {
+
+        this.graph = new JCartesian(600, 600, 0.0, 0.0, 10.0, 20.0);
+        this.add(graph, BorderLayout.CENTER);
+    }
+
+    private void pnlSouth() {
+
+        JPanel pnlSouth = new JPanel(new GridLayout(1, 2));
+
+        JPanel pnlCounter = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        this.lblCounter = new JLabel("0:00");
+        this.lblCounter.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+        pnlCounter.add(this.lblCounter);
+
+        JPanel pnlButtons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        this.btnStartStop = new JButton("START");
+        pnlButtons.add(this.btnStartStop);
+
+        pnlSouth.add(pnlCounter);
+        pnlSouth.add(pnlButtons);
+
+        this.add(pnlSouth, BorderLayout.SOUTH);
+
+        this.btnStartStop.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == this.btnStartStop) {
 
+            if (this.timer.isRunning()) {
+
+                timer.stop();
+                this.btnStartStop.setText("START");
+            }
+            else {
+
+                timer.start();
+                this.btnStartStop.setText("STOP");
+
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new FrmTetris());
+    }
+
 }
--- src/Round.java
+++ src/Round.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class Round {
+import java.awt.*;
+
+public class Round {
+
+    private static int time = 0;
+    private static int anew = 0;
+
+    private static double x1 = 0;
+    private static double x2 = 0;
+    private static double y1 = 0;
+    private static double y2 = 0;
+
+    public enum Block {
+
+        SBLOCK,         //   ##
+                        //  ##
+
+        ZBLOCK,         //  ##
+                        //   ##
+
+        LBLOCK,         //  ###
+                        //  #
+
+        JBLOCK,         //  #
+                        //  ###
+
+        SQUAREBLOCK,    //  ##
+                        //  ##
+
+        IBLOCK,         //  ####
+                        //
+
+        TBLOCK,         //  ###
+                        //   #
+    }
+
+    public Round() {}
+    public static void next() {
+
+        updateTime();
+        if(anew == 0) {
+
+            anew = 1;
+            x1 = 5.0;
+            x2 = 6.0;
+            y1 = 19.0;
+            y2 = 20.0;
+
+            Round.drawNewSBlock();
+        }
+        else {
+
+            FrmTetris.graph.clean();
+
+            y1--;
+            y2--;
+            Round.drawNewSBlock();
+        }
+    }
+
+    private static void updateTime() {
+
+        time++;
+
+        int minutes, seconds = time;
+        for(minutes = 0; seconds >= 60; minutes++)
+            seconds -= 60;
+
+        String zero = "";
+        if(seconds < 10) zero = "0";
+        FrmTetris.lblCounter.setText(minutes + ":" + zero + seconds);
+    }
+
+
+    public static void drawNewSBlock() {
+
+        FrmTetris.graph.fillRect(x1 - 1, y1 - 1, x2 - 1, y2 - 1, Color.GREEN);
+        FrmTetris.graph.fillRect(x1, y1 - 1, x2, y2 - 1, Color.GREEN);
+        FrmTetris.graph.fillRect(x1, y1, x2, y2, Color.GREEN);
+        FrmTetris.graph.fillRect(x1 + 1, y1, x2 + 1, y2, Color.GREEN);
+    }
 }
